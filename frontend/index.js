b1e31843b1f7e6c23fdc193dd0cc18769ded39a7
RightNxw <69996981+RightNxw@users.noreply.github.com>
2021-08-06 20:15:37 -0700

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,184 +1,311 @@
+const _0x4a9b = [
+  "btn",
+  "addTable",
+  "application/json",
+  "Total\x20Price",
+  "src",
+  "container-cards",
+  "textContent",
+  "44829ENEcja",
+  "containerImg",
+  "tbody",
+  "table-sizes-",
+  "name",
+  "centerImg",
+  "build",
+  "center-img",
+  "46843RdeAkB",
+  "contains",
+  "getElementById",
+  "1631072OcHyES",
+  "table",
+  "root",
+  "length",
+  "58qxFxjr",
+  "963321pIXxWi",
+  "name-shoe",
+  "img",
+  "classList",
+  "setImage",
+  "json",
+  "date",
+  "table-hidden",
+  "card",
+  "addEventListener",
+  "title",
+  "push",
+  "table-information",
+  "createElement",
+  "2mqCbmf",
+  "Variant\x20With\x20Stock",
+  "add",
+  "className",
+  "https://andromeda-tools.herokuapp.com/shoesjson",
+  "1051853NJwzye",
+  "image-shoe",
+  "68633gEAOxl",
+  "div",
+  "image",
+  "button",
+  "appendChild",
+  "click",
+  "table-body-information",
+  "vars",
+  "Display\x20Variants",
+  "Release\x20Date",
+  "getHandleClick",
+  "setButton",
+  "5019fUCcwe",
+  "card-",
+  "then",
+  "initCard",
+  "GET",
+  "1DNKgCf",
+  "price",
+];
+function _0x2782(_0x386aa9, _0x6513b) {
+  return (
+    (_0x2782 = function (_0x4a9b90, _0x2782ef) {
+      _0x4a9b90 = _0x4a9b90 - 0xb2;
+      let _0x4c6b95 = _0x4a9b[_0x4a9b90];
+      return _0x4c6b95;
+    }),
+    _0x2782(_0x386aa9, _0x6513b)
+  );
+}
+const _0x248536 = _0x2782;
+(function (_0x5a47e9, _0x3eae0f) {
+  const _0xf4fed5 = _0x2782;
+  while (!![]) {
+    try {
+      const _0x24f5f1 =
+        -parseInt(_0xf4fed5(0xb6)) +
+        parseInt(_0xf4fed5(0xce)) +
+        -parseInt(_0xf4fed5(0xe1)) * -parseInt(_0xf4fed5(0xb3)) +
+        parseInt(_0xf4fed5(0xba)) * parseInt(_0xf4fed5(0xdc)) +
+        parseInt(_0xf4fed5(0xbb)) +
+        parseInt(_0xf4fed5(0xea)) +
+        parseInt(_0xf4fed5(0xd0)) * parseInt(_0xf4fed5(0xc9));
+      if (_0x24f5f1 === _0x3eae0f) break;
+      else _0x5a47e9["push"](_0x5a47e9["shift"]());
+    } catch (_0x5d0e1c) {
+      _0x5a47e9["push"](_0x5a47e9["shift"]());
+    }
+  }
+})(_0x4a9b, 0xdcbce);
 let jsonShoes;
-fetch("https://andromeda-tools.herokuapp.com/shoesjson", {
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
-  method: "GET",
+fetch(_0x248536(0xcd), {
+  headers: { Accept: _0x248536(0xe5), "Content-Type": _0x248536(0xe5) },
+  method: _0x248536(0xe0),
 })
-  .then((res) => res.json())
-  .then((res) => {
-    jsonShoes = res;
+  ["then"]((_0x5599ce) => _0x5599ce[_0x248536(0xc0)]())
+  ["then"]((_0xc5021b) => {
+    jsonShoes = _0xc5021b;
   })
-  .then(() => {
-    class P {
-      constructor(text) {
-        this.p = document.createElement("p");
-        this.p.textContent = text;
-        return this.p;
+  [_0x248536(0xde)](() => {
+    const _0x22039b = _0x248536;
+    class _0x1b1d6e {
+      constructor(_0x38d4af) {
+        const _0x12a876 = _0x2782;
+        return (
+          (this["p"] = document[_0x12a876(0xc8)]("p")),
+          (this["p"][_0x12a876(0xe9)] = _0x38d4af),
+          this["p"]
+        );
       }
     }
-
-    class Div {
+    class _0x1fc227 {
       constructor() {
-        this.div = document.createElement("div");
-        return this.div;
+        const _0x273fb5 = _0x2782;
+        return (
+          (this[_0x273fb5(0xd1)] = document[_0x273fb5(0xc8)](_0x273fb5(0xd1))),
+          this[_0x273fb5(0xd1)]
+        );
       }
     }
-
-    class Img {
-      constructor(url) {
-        this.img = document.createElement("img");
-        this.img.src = url;
-        this.img.classList.add("image-shoe");
-        this.centerImg = new Div();
-        this.centerImg.classList.add("center-img");
-        this.centerImg.appendChild(this.img);
-        this.containerImg = new Div();
-        this.containerImg.classList.add("container-img");
-        this.containerImg.appendChild(this.centerImg);
-        return this.containerImg;
+    class _0x5f5743 {
+      constructor(_0x149f03) {
+        const _0x2b52eb = _0x2782;
+        return (
+          (this[_0x2b52eb(0xbd)] = document["createElement"](_0x2b52eb(0xbd))),
+          (this[_0x2b52eb(0xbd)][_0x2b52eb(0xe7)] = _0x149f03),
+          this[_0x2b52eb(0xbd)][_0x2b52eb(0xbe)][_0x2b52eb(0xcb)](
+            _0x2b52eb(0xcf)
+          ),
+          (this["centerImg"] = new _0x1fc227()),
+          this[_0x2b52eb(0xef)]["classList"][_0x2b52eb(0xcb)](_0x2b52eb(0xb2)),
+          this[_0x2b52eb(0xef)][_0x2b52eb(0xd4)](this[_0x2b52eb(0xbd)]),
+          (this[_0x2b52eb(0xeb)] = new _0x1fc227()),
+          this[_0x2b52eb(0xeb)][_0x2b52eb(0xbe)][_0x2b52eb(0xcb)](
+            "container-img"
+          ),
+          this[_0x2b52eb(0xeb)][_0x2b52eb(0xd4)](this["centerImg"]),
+          this["containerImg"]
+        );
       }
     }
-
-    class TableCell {
-      constructor(text) {
-        this.td = document.createElement("td");
-        this.td.textContent = text;
-        return this.td;
+    class _0x24cda3 {
+      constructor(_0xc5fed4) {
+        const _0x551c64 = _0x2782;
+        return (
+          (this["td"] = document[_0x551c64(0xc8)]("td")),
+          (this["td"][_0x551c64(0xe9)] = _0xc5fed4),
+          this["td"]
+        );
       }
     }
-
-    class TableBody {
-      constructor(rows) {
-        this.tbody = document.createElement("tbody");
-        this.tbody.classList.add("table-body-information");
-        for (const row of rows) this.tbody.appendChild(row);
-        return this.tbody;
+    class _0x472b61 {
+      constructor(_0x446405) {
+        const _0x39ac41 = _0x2782;
+        (this[_0x39ac41(0xec)] = document["createElement"]("tbody")),
+          this[_0x39ac41(0xec)][_0x39ac41(0xbe)][_0x39ac41(0xcb)](
+            _0x39ac41(0xd6)
+          );
+        for (const _0x179c78 of _0x446405)
+          this[_0x39ac41(0xec)]["appendChild"](_0x179c78);
+        return this[_0x39ac41(0xec)];
       }
     }
-
-    class TableRow {
-      constructor(cells) {
-        this.tr = document.createElement("tr");
-        for (const cell of cells) this.tr.appendChild(new TableCell(cell));
-        return this.tr;
+    class _0x20e1aa {
+      constructor(_0x1617f5) {
+        const _0xe05d = _0x2782;
+        this["tr"] = document[_0xe05d(0xc8)]("tr");
+        for (const _0x550d0d of _0x1617f5)
+          this["tr"][_0xe05d(0xd4)](new _0x24cda3(_0x550d0d));
+        return this["tr"];
       }
     }
-
-    class Table {
-      constructor(id, rows, isVisible) {
-        this.table = document.createElement("table");
-        this.table.classList.add("table-information");
-        this.table.id = id;
-
-        const tableRows = [];
-        for (const row of rows) tableRows.push(new TableRow(row));
-
-        this.table.appendChild(new TableBody(tableRows));
-        if (!isVisible) this.table.classList.add("table-hidden");
-        return this.table;
+    class _0x835031 {
+      constructor(_0xb405cd, _0x2600a1, _0x47245a) {
+        const _0x8f8dcc = _0x2782;
+        (this[_0x8f8dcc(0xb7)] = document[_0x8f8dcc(0xc8)]("table")),
+          this[_0x8f8dcc(0xb7)][_0x8f8dcc(0xbe)][_0x8f8dcc(0xcb)](
+            _0x8f8dcc(0xc7)
+          ),
+          (this[_0x8f8dcc(0xb7)]["id"] = _0xb405cd);
+        const _0x6a547f = [];
+        for (const _0x4ffa4d of _0x2600a1)
+          _0x6a547f[_0x8f8dcc(0xc6)](new _0x20e1aa(_0x4ffa4d));
+        this["table"][_0x8f8dcc(0xd4)](new _0x472b61(_0x6a547f));
+        if (!_0x47245a)
+          this["table"]["classList"][_0x8f8dcc(0xcb)](_0x8f8dcc(0xc2));
+        return this[_0x8f8dcc(0xb7)];
       }
     }
-
-    class Button {
-      constructor(cardId, text) {
-        this.id = `button-${cardId}`;
-        this.button = document.createElement("button");
-        this.button.classList.add("btn");
-        this.button.textContent = text;
-        this.button.addEventListener("click", this.getHandleClick(cardId));
-        this.button.id = this.id;
-        return this.button;
+    class _0x2f6bfd {
+      constructor(_0x4e4d18, _0x3d1004) {
+        const _0x5c79ea = _0x2782;
+        return (
+          (this["id"] = "button-" + _0x4e4d18),
+          (this[_0x5c79ea(0xd3)] = document[_0x5c79ea(0xc8)](_0x5c79ea(0xd3))),
+          this[_0x5c79ea(0xd3)]["classList"]["add"](_0x5c79ea(0xe3)),
+          (this[_0x5c79ea(0xd3)]["textContent"] = _0x3d1004),
+          this[_0x5c79ea(0xd3)][_0x5c79ea(0xc4)](
+            _0x5c79ea(0xd5),
+            this["getHandleClick"](_0x4e4d18)
+          ),
+          (this[_0x5c79ea(0xd3)]["id"] = this["id"]),
+          this[_0x5c79ea(0xd3)]
+        );
       }
-
-      getHandleClick(cardId) {
-        const handleClick = () => {
-          const tableToggle = document.getElementById(`table-sizes-${cardId}`);
-          const btn = document.getElementById(this.id);
-          if (tableToggle.classList.contains("table-hidden")) {
-            tableToggle.className = "table-information";
-            btn.textContent = "Hide Sizes";
-          } else {
-            tableToggle.classList.add("table-hidden");
-            btn.textContent = "Display Variants";
-          }
+      [_0x22039b(0xda)](_0xcbe576) {
+        const _0xaa656e = () => {
+          const _0x30cbda = _0x2782,
+            _0x4b99c0 = document[_0x30cbda(0xb5)](_0x30cbda(0xed) + _0xcbe576),
+            _0x3003f8 = document[_0x30cbda(0xb5)](this["id"]);
+          _0x4b99c0["classList"][_0x30cbda(0xb4)](_0x30cbda(0xc2))
+            ? ((_0x4b99c0[_0x30cbda(0xcc)] = "table-information"),
+              (_0x3003f8[_0x30cbda(0xe9)] = "Hide\x20Sizes"))
+            : (_0x4b99c0[_0x30cbda(0xbe)][_0x30cbda(0xcb)]("table-hidden"),
+              (_0x3003f8[_0x30cbda(0xe9)] = _0x30cbda(0xd8)));
         };
-
-        return handleClick;
+        return _0xaa656e;
       }
     }
-
-    class Card {
-      constructor(id, title, url) {
-        this.id = `card-${id}`;
-        this.initCard();
-        this.setTitle(title);
-        this.setImage(url);
-        this.setButton(id);
+    class _0x1b2a69 {
+      constructor(_0x155e67, _0x261959, _0x232578) {
+        const _0x27b334 = _0x22039b;
+        (this["id"] = _0x27b334(0xdd) + _0x155e67),
+          this[_0x27b334(0xdf)](),
+          this["setTitle"](_0x261959),
+          this[_0x27b334(0xbf)](_0x232578),
+          this[_0x27b334(0xdb)](_0x155e67);
       }
-
-      build() {
-        return this.card;
+      [_0x22039b(0xf0)]() {
+        const _0x142929 = _0x22039b;
+        return this[_0x142929(0xc3)];
       }
-
-      initCard() {
-        this.card = new Div();
-        this.card.classList.add("card");
-        this.card.id = this.id;
+      [_0x22039b(0xdf)]() {
+        const _0x35b39d = _0x22039b;
+        (this[_0x35b39d(0xc3)] = new _0x1fc227()),
+          this[_0x35b39d(0xc3)]["classList"][_0x35b39d(0xcb)](_0x35b39d(0xc3)),
+          (this["card"]["id"] = this["id"]);
       }
-
-      setTitle(title) {
-        this.title = new P(title);
-        this.title.classList.add("name-shoe");
-        this.card.appendChild(this.title);
+      ["setTitle"](_0x2889df) {
+        const _0x356480 = _0x22039b;
+        (this[_0x356480(0xc5)] = new _0x1b1d6e(_0x2889df)),
+          this[_0x356480(0xc5)][_0x356480(0xbe)][_0x356480(0xcb)](
+            _0x356480(0xbc)
+          ),
+          this[_0x356480(0xc3)][_0x356480(0xd4)](this[_0x356480(0xc5)]);
       }
-
-      setImage(url) {
-        this.image = new Img(url);
-        this.card.appendChild(this.image);
+      [_0x22039b(0xbf)](_0x406908) {
+        const _0x23e8a4 = _0x22039b;
+        (this["image"] = new _0x5f5743(_0x406908)),
+          this[_0x23e8a4(0xc3)][_0x23e8a4(0xd4)](this[_0x23e8a4(0xd2)]);
       }
-
-      addTable(id, rows, isVisible = true) {
-        this.table = new Table(`${id}-${this.id}`, rows, isVisible);
-        this.card.appendChild(this.table);
+      ["addTable"](_0x273d7d, _0x194e78, _0x10b333 = !![]) {
+        const _0x4bbb2c = _0x22039b;
+        (this[_0x4bbb2c(0xb7)] = new _0x835031(
+          _0x273d7d + "-" + this["id"],
+          _0x194e78,
+          _0x10b333
+        )),
+          this["card"][_0x4bbb2c(0xd4)](this[_0x4bbb2c(0xb7)]);
       }
-
-      setButton() {
-        this.button = new Button(this.id, "Display Variants");
-        this.card.appendChild(this.button);
+      [_0x22039b(0xdb)]() {
+        const _0x181fa8 = _0x22039b;
+        (this["button"] = new _0x2f6bfd(this["id"], _0x181fa8(0xd8))),
+          this["card"]["appendChild"](this[_0x181fa8(0xd3)]);
       }
     }
-
-    const cards = [];
-
-    jsonShoes.forEach((shoe, idx) => {
-      const tableInformation = [
-        ["Total Price", "Release Date", " Total Stock"],
-        [shoe.price, shoe.date, shoe.stock],
-      ];
-
-      const tableSizes = [["Variants", "Variant With Stock", "Stock"]];
-
-      const vars = shoe.vars[0].split("\n");
-      const sizeVars = shoe.sizeVars[0].split("\n");
-      const stock = shoe.stockList[0].split("\n");
-
-      for (let i = 0; i < vars.length - 1; i++)
-        tableSizes.push([vars[i], sizeVars[i], stock[i]]);
-
-      const card = new Card(idx, shoe.name, shoe.image);
-      card.addTable("table-information", tableInformation);
-      card.addTable("table-sizes", tableSizes, false);
-      cards.push(card.build());
+    const _0x1983ef = [];
+    jsonShoes["forEach"]((_0x3ca73a, _0x153aa5) => {
+      const _0x1ea5a0 = _0x22039b,
+        _0x22c9ff = [
+          [_0x1ea5a0(0xe6), _0x1ea5a0(0xd9), "\x20Total\x20Stock"],
+          [
+            _0x3ca73a[_0x1ea5a0(0xe2)],
+            _0x3ca73a[_0x1ea5a0(0xc1)],
+            _0x3ca73a["stock"],
+          ],
+        ],
+        _0xe18713 = [["Variants", _0x1ea5a0(0xca), "Stock"]],
+        _0x191498 = _0x3ca73a[_0x1ea5a0(0xd7)][0x0]["split"]("\x0a"),
+        _0x29d60f = _0x3ca73a["sizeVars"][0x0]["split"]("\x0a"),
+        _0x1d32fe = _0x3ca73a["stockList"][0x0]["split"]("\x0a");
+      for (
+        let _0x26323c = 0x0;
+        _0x26323c < _0x191498[_0x1ea5a0(0xb9)] - 0x1;
+        _0x26323c++
+      )
+        _0xe18713[_0x1ea5a0(0xc6)]([
+          _0x191498[_0x26323c],
+          _0x29d60f[_0x26323c],
+          _0x1d32fe[_0x26323c],
+        ]);
+      const _0x57f361 = new _0x1b2a69(
+        _0x153aa5,
+        _0x3ca73a[_0x1ea5a0(0xee)],
+        _0x3ca73a[_0x1ea5a0(0xd2)]
+      );
+      _0x57f361[_0x1ea5a0(0xe4)](_0x1ea5a0(0xc7), _0x22c9ff),
+        _0x57f361[_0x1ea5a0(0xe4)]("table-sizes", _0xe18713, ![]),
+        _0x1983ef[_0x1ea5a0(0xc6)](_0x57f361[_0x1ea5a0(0xf0)]());
     });
-
-    const root = document.getElementById("root");
-
-    const containerCards = new Div();
-    containerCards.classList.add("container-cards");
-
-    for (const card of cards) containerCards.appendChild(card);
-
-    root.appendChild(containerCards);
+    const _0x3f345f = document[_0x22039b(0xb5)](_0x22039b(0xb8)),
+      _0x208578 = new _0x1fc227();
+    _0x208578[_0x22039b(0xbe)]["add"](_0x22039b(0xe8));
+    for (const _0x4d4fc8 of _0x1983ef) _0x208578[_0x22039b(0xd4)](_0x4d4fc8);
+    _0x3f345f["appendChild"](_0x208578);
   });
